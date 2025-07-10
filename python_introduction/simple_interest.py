1a8f27dccaadc0eee000eb62b2f178e89d16ac36
Chidera001-dev <nnamanichideraemmanuel7@gmail.com>
2025-07-10 12:46:40 +0100

    Add simple_interest.py to python_introduction

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000     # The amount of money invested
+rate = 0.05          # 5% interest (written as decimal)
+time = 3             # 3 years 
+
+interest = principal * rate * time 
+
+print("The simple interest is:", interest)
