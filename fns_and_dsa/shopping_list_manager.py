aba23512daf2ea507cbf25bbe09af4cbbdf2789d
Chidera001-dev <nnamanichideraemmanuel7@gmail.com>
2025-07-25 17:06:42 +0100

    added a files

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,12 +13,12 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter item name to add: ")
+            item = input("Enter item to add: ")
             shopping_list.append(item)
             print(f"'{item}' added to your list.")
         
         elif choice == '2':
-            item = input("Enter item name to remove: ")
+            item = input("Enter item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
                 print(f"'{item}' removed from your list.")
