b58647235c2a0a620b0bfd15256f93f3b9cbfaf5
Chidera001-dev <nnamanichideraemmanuel7@gmail.com>
2025-07-25 14:29:35 +0100

    addes a file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,9 +7,9 @@ def perform_operation(num1, num2, operation):
     match operation:
         case "add":
             return num1 + num2
-        case "substract":
+        case "subtract":
             return num1 - num2
-        case "multipy":
+        case "multiply":
             return num1 * num2
         case "divide":
             if num2 == 0:
